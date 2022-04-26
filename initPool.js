c890c586f0f287f2a52b948d1d3157b9a75876e4
Liam Leung <liam.leung@binery.co>
2022-04-26 20:43:58 +0800

    changed something on line 27 of index.js changed something on line 27 of index.js changed something on line 27 of index.js changed something on line 27 of index.js changed something on line 27 of index.js

--- initPool.js
+++ initPool.js
@@ -4,7 +4,6 @@ const { Pool } = pg;
 
 let pgConnectionConfigs;
 
-// test to see if the env var is set. Then we know we are in Heroku
 if (process.env.DATABASE_URL) {
   // pg will take in the entire value and use it to connect
   pgConnectionConfigs = {
